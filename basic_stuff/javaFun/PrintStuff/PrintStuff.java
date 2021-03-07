e0de99e5b4f2f130f4b9f32fa5eacd6ff58c1856
michellenesbitt <mn218@nerdymail.com>
2021-03-07 11:11:59 -0500

    mn

---
+++ basic_stuff/javaFun/PrintStuff/PrintStuff.java
@@ -0,0 +1,9 @@
+public class PrintStuff {
+    public static void main(String[] args) {
+
+        System.out.println("My name is Michelle Nesbitt");
+        System.out.println("I am 21 years old");
+        System.out.println("My hometown is Levittown, NY");
+        
+    }
+}
