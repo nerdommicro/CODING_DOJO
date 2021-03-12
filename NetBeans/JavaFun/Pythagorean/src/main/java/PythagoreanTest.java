ab5a0b72d053841ccd6545d28b30365faa56381a
michellenesbitt <mn218@nerdymail.com>
2021-03-12 16:36:12 -0500

    mn

--- NetBeans/JavaFun/Pythagorean/src/main/java/PythagoreanTest.java
+++ NetBeans/JavaFun/Pythagorean/src/main/java/PythagoreanTest.java
@@ -2,6 +2,7 @@
 public class PythagoreanTest {
     
     public static void main(String[] args) {
+        
         Pythagorean p = new Pythagorean();
         double result = p.calculateHypotenuse(5, 6);
         System.out.print(result);
